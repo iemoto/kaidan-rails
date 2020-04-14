d45fe6382e70f5449ba17ec79133dae7e30b9a42
iemoto <reyryuuu@gmail.com>
2020-04-14 21:01:07 +0900

    hashForm

--- app/assets/javascripts/rate.js
+++ app/assets/javascripts/rate.js
@@ -11,23 +11,9 @@ $(function(){
       `
       targetTable.append(html);
   }
-  function lastBuildTable(tabel){
-    var html = `
-      <tr>
-        <td>合計</td>
-        <td>${tabel}</td>
-        <td>${tabel}</td>
-        <td>${tabel}</td>
-      </tr>
-      `
-      targetTable.append(html);
-  }
+
   $('#trial_cal').on('click',function(e){
     e.preventDefault();
-    var hashForm = {};
-    hashForm.test = $('input[name = "method"]:checked').val();
-
-
     $.ajax({
       method: 'GET',
       url: 'api/rates',
@@ -37,14 +23,14 @@ $(function(){
       $('#target_table').children().remove();
       buildTable(1);
       buildTable(2);
-      lastBuildTable(3);
+      var hashForm = {};
+      hashForm.method = $('input[name = "method"]:checked').val();
+      hashForm.serviceLife = $('#ser_life').val();
+      hashForm.price = $('.price-form-text').val();
+      hashForm.startMonth = $('#sta_month').val();
+      hashForm.operationMonth = $('#ope_month').val();
 
-      console.log(rates);
-      console.log(hashForm.test);
-      console.log($('input[name = "method"]:checked').val());
-      console.log($('.price-form-text').val());
-      console.log($('#sta_month').val());
-      console.log($('#ope_month').val());  
+
     })
     .fail(function(){
       console.log('Error')
