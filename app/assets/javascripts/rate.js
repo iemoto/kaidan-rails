b32780e8ec7f411bbfafaa623ac11cfe3fa71c81
iemoto <reyryuuu@gmail.com>
2020-04-14 15:29:41 +0900

    jquery

--- app/assets/javascripts/rate.js
+++ app/assets/javascripts/rate.js
@@ -1,4 +1,10 @@
 $(function(){
-$('#method').on('radio')
-
+  $('#trial_cal').on('click',function(e){
+    e.preventDefault();
+    console.log('ok');
+    console.log($('input[name = "method"]:checked').val());
+    console.log($('.price-form-text').val());
+    console.log($('#sta_month').val());
+    console.log($('#ope_month').val());
+  })
 })
