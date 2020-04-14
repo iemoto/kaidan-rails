f809ecb95a2bb9b9f0b33315a4a996838d152643
iemoto <reyryuuu@gmail.com>
2020-04-14 21:43:39 +0900

    int化

--- app/assets/javascripts/rate.js
+++ app/assets/javascripts/rate.js
@@ -12,6 +12,12 @@ $(function(){
       targetTable.append(html);
   }
 
+  function straightLine( serviceLife , price ){
+    
+
+  }
+
+
   $('#trial_cal').on('click',function(e){
     e.preventDefault();
     $.ajax({
@@ -21,24 +27,23 @@ $(function(){
     })
     .done(function(rates){
       $('#target_table').children().remove();
+      console.log(rates[0]);
       buildTable(1);
       buildTable(2);
       var hashForm = {};
-      hashForm.method = $('input[name = "method"]:checked').val();
-      hashForm.serviceLife = $('#ser_life').val();
-      hashForm.price = $('.price-form-text').val();
-      hashForm.startMonth = $('#sta_month').val();
-      hashForm.operationMonth = $('#ope_month').val();
+      hashForm.method = parseInt($('input[name = "method"]:checked').val(), 10);
+      hashForm.serviceLife = parseInt($('#ser_life').val(), 10);
+      hashForm.price = parseInt($('.price-form-text').val(), 10);
+      hashForm.startMonth = parseInt($('#sta_month').val(), 10);
+      hashForm.operationMonth = parseInt($('#ope_month').val(), 10);
 
       switch (hashForm.method){
-        case '0':
+        case 0:
           console.log(0);
           break;
-        case '1':
-          console.log(1);
+        case 1:
           break;
-        case '2':
-          console.log('OK');
+        case 2:
           break;
       }
     })
