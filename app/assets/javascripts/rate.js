f3b1f8874753442491449802cd88b5087a46a514
iemoto <reyryuuu@gmail.com>
2020-04-14 20:27:18 +0900

    table function create

--- app/assets/javascripts/rate.js
+++ app/assets/javascripts/rate.js
@@ -1,5 +1,33 @@
 $(function(){
+  var targetTable = $('#target_table');
+  function buildTable(tabel){
+    var html = `
+      <tr>
+        <td>${tabel}</td>
+        <td>${tabel}</td>
+        <td>${tabel}</td>
+        <td>${tabel}</td>
+      </tr>
+      `
+      targetTable.append(html);
+  }
+  function lastBuildTable(tabel){
+    var html = `
+      <tr>
+        <td>合計</td>
+        <td>${tabel}</td>
+        <td>${tabel}</td>
+        <td>${tabel}</td>
+      </tr>
+      `
+      targetTable.append(html);
+  }
   $('#trial_cal').on('click',function(e){
+    $('#target_table').children().remove();
+    buildTable(1);
+    buildTable(2);
+
+
     e.preventDefault();
     $.ajax({
       method: 'GET',
@@ -12,6 +40,9 @@ $(function(){
       console.log($('.price-form-text').val());
       console.log($('#sta_month').val());
       console.log($('#ope_month').val());  
+    })
+    .fail(function(){
+      console.log('Error')
     })
   })
 })
