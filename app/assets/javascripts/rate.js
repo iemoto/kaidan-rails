5884b06fc1e4972bf92a95fc7332c61ffaca7f35
iemoto <reyryuuu@gmail.com>
2020-04-14 16:45:47 +0900

    j add

--- app/assets/javascripts/rate.js
+++ app/assets/javascripts/rate.js
@@ -1,11 +1,6 @@
 $(function(){
   $('#trial_cal').on('click',function(e){
     e.preventDefault();
-    console.log('ok');
-    console.log($('input[name = "method"]:checked').val());
-    console.log($('.price-form-text').val());
-    console.log($('#sta_month').val());
-    console.log($('#ope_month').val());
     $.ajax({
       method: 'GET',
       url: 'api/rates',
--- app/controllers/api/rates_controller.rb
+++ app/controllers/api/rates_controller.rb
@@ -1,6 +1,5 @@
 class Api::RatesController < ApplicationController
   def index
     @rates = Rate.all
-    binding.pry
   end
 end
