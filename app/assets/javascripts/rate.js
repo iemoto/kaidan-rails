0efa776c270a7d2a6932dcb155b9cf8114dd47b7
iemoto <reyryuuu@gmail.com>
2020-04-16 14:07:17 +0900

    定率法スタート

--- app/assets/javascripts/rate.js
+++ app/assets/javascripts/rate.js
@@ -28,16 +28,62 @@ $(function(){
   }
   function straightLine( formObj , rateObj ){
 
+    if (typeof rateObj != 'object'|| typeof formObj != 'object') return false;
+
     var table = {};
     if (formObj.startMonth > formObj.operationMonth){
       table.depMonth = formObj.startMonth - formObj.operationMonth;
-      console.log(table.depMonth);
     }else{
       table.depMonth = 12 - (formObj.operationMonth - formObj.startMonth);
-      console.log(table.depMonth);
     }
     let temp;
+
+    for( var i = 1; i < formObj.serviceLife + 1; i++ ) {
+      table.id = i;
+      table.beginValue = table.endValue;
+
+      if (i === 1){
+        table.beginValue = formObj.price;
+        table.depValue = table.beginValue * rateObj.straight_line; 
+        table.depValue = roundJudge( table.depValue , formObj.round );
+        temp = table.depValue;
+        if (!isNaN(formObj.startMonth) || !isNaN(formObj.operationMonth)){
+          table.depValue = (table.depValue / 12) * table.depMonth;
+          table.depValue = roundJudge( table.depValue , formObj.round );
+        };
+      };
+
+      if (table.depValue >= table.beginValue){ table.depValue = table.beginValue - 1;};
+
+      table.endValue = table.beginValue - table.depValue; 
+
+      appendTable(table);
+
+      if (i === 1){
+        table.depValue = temp;
+      };
+
+      if (i === formObj.serviceLife && table.endValue > 1){
+        table.id = i + 1;
+        table.beginValue = table.endValue;
+        table.depValue = table.beginValue - 1;
+        table.endValue = table.beginValue - table.depValue;
+        appendTable(table);
+      }
+    }
+  }
+  function fiexdRate( formObj , rateObj ){
+
     if (typeof rateObj != 'object'|| typeof formObj != 'object') return false;
+    
+    var table = {};
+    if (formObj.startMonth > formObj.operationMonth){
+      table.depMonth = formObj.startMonth - formObj.operationMonth;
+    }else{
+      table.depMonth = 12 - (formObj.operationMonth - formObj.startMonth);
+    }
+    let temp;
+
 
     for( var i = 1; i < formObj.serviceLife + 1; i++ ) {
       table.id = i;
@@ -74,7 +120,6 @@ $(function(){
     }
   }
 
-
   $('#trial_cal').on('click',function(e){
     e.preventDefault();
     $.ajax({
@@ -96,9 +141,10 @@ $(function(){
 
       switch (hashForm.method){
         case 0:
-          straightLine( hashForm , rates[hashForm.serviceLife - 1] )
+          straightLine( hashForm , rates[hashForm.serviceLife - 1] );
           break;
         case 1:
+          fiexdRate( hashForm , rates[hashForm.serviceLife - 1] );
           break;
         case 2:
           break;
