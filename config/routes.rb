3d7343f2c34c02b0ea28084c0ea8c22fc81aeb38
iemoto <reyryuuu@gmail.com>
2020-04-16 15:09:30 +0900

    rote fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 #
 #                    Prefix Verb URI Pattern                                                                              Controller#Action
 #                      root GET  /                                                                                        boards#index
+#                    boards GET  /boards(.:format)                                                                        boards#index
 #                 api_rates GET  /api/rates(.:format)                                                                     api/rates#index {:format=>/json/}
 #        rails_service_blob GET  /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
 # rails_blob_representation GET  /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
@@ -12,6 +13,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'boards#index'
+  resources :boards, only: [:index]
+
   namespace :api, format: 'json' do
     resources :rates, only: [:index]
   end
